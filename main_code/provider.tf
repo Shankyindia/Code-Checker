a9d709e6c6ed67ad17ca30b5d489be97973f9264
Shankyindia <s.shekhar2197@gmail.com>
2025-08-15 13:53:48 +0530

    try

---
+++ main_code/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "Shashank-rg"
+    storage_account_name = "shankysta"
+    container_name       = "shankycon"
+    key                  = "terraform.tfstate"
+    }  
+  }
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "3be29ba2-b31c-4fbd-a7bb-c51bf50ef740"
+}
