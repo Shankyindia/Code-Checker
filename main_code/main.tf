e4c122573d9d11e9aa083f6501b8724fc7ae002d
Shankyindia <s.shekhar2197@gmail.com>
2025-08-15 17:17:33 +0530

    try

--- main_code/main.tf
+++ main_code/main.tf
@@ -1,12 +1,12 @@
 module "rg" {
   source   = "../infra_code/rg"
-  rg_name  = var.rg_name
-  location = var.location
+  rg_name  = "rsb-rg"
+  location = "West Europe"
 }
 module "storage_account" {
   depends_on           = [module.rg]
   source               = "../infra_code/storage_account"
-  storage_account_name = var.storage_account_name
-  rg_name              = var.rg_name
-  location             = var.location
+  storage_account_name = "rsbstorage"
+  rg_name              = "rsb-rg"
+  location             = "West Europe"
 }
