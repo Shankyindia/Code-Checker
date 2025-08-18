808a253334ff14a6bb879b941dd9524935399b58
Shankyindia <s.shekhar2197@gmail.com>
2025-08-18 22:29:30 +0530

    new

--- main_code/main.tf
+++ main_code/main.tf
@@ -1,12 +1,12 @@
 module "rg" {
   source   = "../infra_code/rg"
-  rg_name  = "rsb-rg"
-  location = "West Europe"
+  rg_name  = var.rg_name
+  location = var.location
 }
 module "storage_account" {
   depends_on           = [module.rg]
   source               = "../infra_code/storage_account"
-  storage_account_name = "rsbstorage"
-  rg_name              = "rsb-rg"
-  location             = "West Europe"
+  storage_account_name = var.storage_account_name
+  rg_name              = var.rg_name
+  location             = var.location
 }
--- main_code/variable.tf
+++ main_code/variable.tf
@@ -1,12 +1,15 @@
 variable "rg_name" {
   description = "The name of the resource group"
   type        = string
+  default     = "rsb-rg"
 }
 variable "location" {
   description = "The location of the resource group"
   type        = string
+  default     = "West Europe"
 }
 variable "storage_account_name" {
   description = "The name of the storage account"
   type        = string
+  default     = "rsbstorage"
 }
