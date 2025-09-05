1ced4cd7668df5db43109503d1b1a9356023f0b3
Shankyindia <s.shekhar2197@gmail.com>
2025-09-05 20:05:50 +0530

    honest

--- main_code/provider.tf
+++ main_code/provider.tf
@@ -5,16 +5,16 @@ terraform {
       version = "4.40.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "Shashank-rg"
-    storage_account_name = "shankysta"
-    container_name       = "shankycon"
-    key                  = "terraform.tfstate"
-  }
-}
+#   backend "azurerm" {
+#     resource_group_name  = "Shashank-rg"
+#     storage_account_name = "shankysta"
+#     container_name       = "shankycon"
+#     key                  = "terraform.tfstate"
+#   }
+# }
 
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = "3be29ba2-b31c-4fbd-a7bb-c51bf50ef740"
+  subscription_id = "85bce315-2d40-4002-a3d1-b7f28202855c"
 }
